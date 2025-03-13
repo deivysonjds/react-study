6517f6a22875dcc9841bf80b6da0d3bf68a5e8c6
Deivyson J. Silva <139878479+deivysonjds@users.noreply.github.com>
2025-03-13 04:30:09 +0000

    atv 1 finalizada

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,8 +1,37 @@
-import Image from "next/image";
-// import Header from "./components/header";
+"use client"
+import { useState } from "react"
+
+const Dado = ({ valor }) => {  
+
+  const imagens = {  
+      1: '/img1.jpg',  
+      2: '/img2.jpg',  
+      3: '/img3.jpg',  
+      4: '/img4.jpg',  
+      5: '/img5.jpg',  
+      6: '/img6.jpg',  
+  };  
+
+  return (  
+      <div>  
+          <img src={imagens[valor]} alt={`Dado com valor ${valor}`} />  
+      </div>  
+  );  
+};
 
 export default function Home(){
-  return <div>
-      <h1>Olá, turma!</h1>
-    </div>
+  const [valorDado, setValorDado] = useState(1);  
+
+    const gerarNumeroAleatorio = () => {  
+        const numeroAleatorio = Math.floor(Math.random() * 6) + 1;  
+        setValorDado(numeroAleatorio); 
+    };  
+
+    return (  
+        <div>  
+            <h1>Lançar o Dado</h1>  
+            <Dado valor={valorDado} />  
+            <button onClick={gerarNumeroAleatorio}>Lançar Dado</button>  
+        </div>  
+    );
 }
